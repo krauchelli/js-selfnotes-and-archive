58e8b147188d0244cf2a547f8215205bfc8bcc20
krauchelli <mekaku26shi@gmail.com>
2023-08-29 07:20:38 +0700

    adding files to "Asynchronous" folder

---
+++ Asynchronous/pakeSetTimeout.js
@@ -0,0 +1,9 @@
+//intinya pake setTimeout function gunanya untuk memberikank interval pada proses yang diinginkan dengan waktu yang diperlukan
+console.log("Welcome!");
+
+setTimeout(() => {
+    console.log("Lagged response")
+}, 1000);
+
+
+console.log("Anything i can help with? ");
---
+++ Asynchronous/withCallback.js
@@ -0,0 +1,6 @@
+/**
+ * Callback merupakan fungsi yang akan disisipkan pada argumen fungsi asynchronous
+ * dan akan dipanggil ketika proses dinyatakan selesai.
+ *
+ * Fungsi callback akan membawa nilai-nilai yang dihasilkan dari proses asynchronous sehingga bisa kita manfaatkan nilai nya*/
+
